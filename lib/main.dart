13b225dbfa81cf6bc9d68941e27320867323c86f
danielp67 <daniel.67100@hotmail.fr>
2024-03-01 13:49:05 +0100

    add navigation rail

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -49,52 +50,86 @@ class MyAppState extends ChangeNotifier {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current; 
+    return Scaffold(
+      body: Row(
+        children: [
+          Expanded(
+            child: SafeArea(
+              child: NavigationRail(
+                extended: true,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: 0,
+                onDestinationSelected: (value) {
+                  print('selected: $value');
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-  IconData icon;
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
     if (appState.favorites.contains(pair)) {
       icon = Icons.favorite;
     } else {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('A random AWSOME idea:'),
-            Text(appState.current.asLowerCase),
-            BigCard(pair: pair),  
-            SizedBox(height: 50),
-              Row(
-                mainAxisSize: MainAxisSize.min, 
-                children: [
-                  ElevatedButton.icon(
-                    onPressed: () {
-                    appState.toggleFavorite();
-                  //  print('button pressed!');
-                  },
-                    icon: Icon(icon), 
-                    label: Text('Like')
-                    ),
-                  SizedBox(width: 50),
-                  ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  //  print('button pressed!');
-                  },
-                  child: Text('Next'),
-                              ),
-                ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
               ),
-          ],
-        ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
+
 
 class BigCard extends StatelessWidget {
   const BigCard({
